4d3a4202085a7d22ef3572a57a90df85d4702fe4
Aron Rissato <aronhrissato@gmail.com>
2025-09-18 17:26:39 -0300

    Starting first test returning red.

---
+++ CloudCustomers.API/Controllers/UsersController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Mvc;
+namespace CloudCustomers.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsersController : ControllerBase
+{
+    public UsersController()
+    {
+    }
+
+    [HttpGet(Name = "GetUsers")]
+    public async Task<IActionResult> Get()
+    {
+        return null;
+    }
+}
---
+++ CloudCustomers.UnitTests/Systems/Controllers/TestUsersController.cs
@@ -0,0 +1,20 @@
+using CloudCustomers.API.Controllers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Xunit;
+
+namespace CloudCustomers.UnitTests.Systems.Controllers;
+
+public class TestUsersController
+{
+    [Fact]
+    public async Task Get_OnSuccess_ReturnsStatusCode200()
+    {
+        // Arrange
+        var sut = new UsersController();
+        // Act
+        var result = (OkObjectResult)await sut.Get();
+        // Assert
+        result.StatusCode.Should().Be(200);
+    }
+}
